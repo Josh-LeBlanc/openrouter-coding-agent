e9894269298f1dff4c351255aaa167de8ebf4ffd
Josh LeBlanc <joshcapitals07@gmail.com>
2025-05-01 20:27:17 +0200

    stopping work on this for now

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# NOTICE
+not working on this any more, too rate limited. trying with ollama instead, then may come back :)
 # openrouter-coding-agent
 coding agent that uses openrouter because I am a brokie that needs free LLM APIs.
 
@@ -6,5 +8,11 @@ blog that I'm following is in go using anthropic api, we're gonna port it.
 i was naive and thought openrouter had a high enough rate limit on their APIs to support an agent. they do not. it is 50 requests per day.
 
 so, this will now also support ollama. qwen3 came out yesterday so we're gonna use that 8b model. one different thing about it is that it is a reasoner so we have a think tag to play with as well.
+# todo:
+implementing tool calls:
+[openai docs](https://platform.openai.com/docs/guides/function-calling?api-mode=responses#handling-function-calls)
+perplexity says you can use the tool calls with `client.chat.completions.create`, and if there are tool calls they will be in
+- __AYO: use the ollama python package instead of openai__
+`response.choices[0].message.tool_calls` so check if that is not an empty array then handle the function call
 ## resources
 [thorsten ball blog](https://ampcode.com/how-to-build-an-agent)
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from openai import OpenAI
 from ansi_codes import *
 import os
-
-ollama = True
+import requests
 
 def get_user_message():
     return input()
 
+def get_weather(latitude, longitude):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+    data = response.json()
+    return data['current']['temperature_2m']
+
 class Agent:
 
-    model = "tngtech/deepseek-r1t-chimera:free" if not ollama else "qwen3"
+    model = "tngtech/deepseek-r1t-chimera:free"
 
     def __init__(self, client: OpenAI, get_user_message):
         self.client = client
@@ -34,7 +38,8 @@ class Agent:
                 "content": user_input
                 })
 
-            message = self.run_inference(conversation)
+            # message = self.run_inference(conversation)
+            message = self.run_tool(conversation)
             conversation.append({
                 "role": "assistant",
                 "content": message
@@ -48,27 +53,58 @@ class Agent:
                 model=self.model,
                 messages=conversation,
                 timeout=90,
-                temperature=0.0
-                ).choices[0].message.content
-        if self.model == "qwen3":
-            if not response:
+                temperature=0.0,
+                )
+        content = response.choices[0].message.content
+        if not content:
+            return "Error getting response."
+        if "<think>" in content:
+            if not content:
                 return "Error getting response."
-            parts = response.split("</think>\n\n")
+            parts = content.split("</think>\n\n")
             think = parts[0][8:-1]
-            response = parts[1]
-        return response
+            content = parts[1]
+        return content
+
+    def run_tool(self, conversation):
+        tools = [{
+            "type": "function",
+            "name": "get_weather",
+            "description": "Get current temperature for a given location.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "City and country e.g. Bogotá, Colombia"
+                        }
+                    },
+                "required": [
+                    "location"
+                    ],
+                "additionalProperties": False
+                }
+            }]
+        response =  self.client.chat.completions.create(
+                model=self.model,
+                messages=conversation,
+                timeout=90,
+                temperature=0.0,
+                tools=tools
+                )
+
+        if not response:
+            return "Response not received"
+
+        print(response.__init__)
+
+
 
 def main():
-    if ollama:
-        client = OpenAI(
-                base_url="http://localhost:11434/v1/",
-                api_key="ollama",
-                )
-    else:
-        client = OpenAI(
-                base_url="https://openrouter.ai/api/v1",
-                api_key=os.getenv("OPENROUTER_API_KEY"),
-                )
+    client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+            )
 
     agent = Agent(client, get_user_message)
     agent.run()
